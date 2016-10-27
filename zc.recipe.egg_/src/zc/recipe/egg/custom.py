jerome-nexedi/buildout

commit aba00feb5cc694be6da433d936e29c94f9ae0416

zc.recipe.egg: Support setup-eggs in :develop.

zc.recipe.egg_/src/zc/recipe/egg/custom.py
@@ -44,6 +44,7 @@ class Base:
     def install(self):
         self._set_environment()
         try:
+            self._install_setup_eggs()
             return self._install()
         finally:
             self._restore_environment()
@@ -70,6 +71,24 @@ class Base:
                     del os.environ[key]
                 except KeyError:
                     pass
+
+    def _install_setup_eggs(self):
+        options = self.options
+        setup_eggs = [
+            r.strip()
+            for r in options.get('setup-eggs', '').split('\n')
+            if r.strip()]
+        if setup_eggs:
+            ws = zc.buildout.easy_install.install(
+                setup_eggs, options['_e'],
+                links=self.links,
+                index=self.index,
+                executable=sys.executable,
+                path=[options['_d'], options['_e']],
+                newest=self.newest,
+                )
+            extra_path = os.pathsep.join(ws.entries)
+            os.environ['PYTHONEXTRAPATH'] = extra_path
 
     def _get_patch_dict(self, options, distribution):
         patch_dict = {}
@@ -134,22 +153,6 @@ class Custom(Base):
 
         distribution = options.get('egg', options.get('eggs', self.name)
                                    ).strip()
-
-        setup_eggs = [
-            r.strip()
-            for r in options.get('setup-eggs', '').split('\n')
-            if r.strip()]
-        if setup_eggs:
-            ws = zc.buildout.easy_install.install(
-                setup_eggs, options['_e'],
-                links=self.links,
-                index=self.index,
-                executable=sys.executable,
-                path=[options['_d'], options['_e']],
-                newest=self.newest,
-                )
-            extra_path = os.pathsep.join(ws.entries)
-            self.environment['PYTHONEXTRAPATH'] = os.environ['PYTHONEXTRAPATH'] = extra_path
 
         patch_dict = self._get_patch_dict(options, distribution)
         return zc.buildout.easy_install.build(

commit 53997edacca4ebb4d070b63388b245b3afd775e7

zc.recipe.egg: Fixup! Support setup-eggs in :develop.
This change fixes bug introduced in https://lab.nexedi.cn/nexedi/slapos.buildout/commit/aba00feb5cc694be6da433d936e29c94f9ae0416 for develop.

As _install_setup_eggs is part of Base class, some code
from Custom.__init__ had to be moved to Base.__init__ as those were
required by _install_setup_eggs implementation as self.link, options['_e'],
self.newest and so on.

Without this change zc.recipe.egg:develop fail to process.

zc.recipe.egg_/src/zc/recipe/egg/custom.py
@@ -29,6 +29,8 @@ class Base:
         self.name, self.options = name, options
 
         options['_d'] = buildout['buildout']['develop-eggs-directory']
+        options['_e'] = buildout['buildout']['eggs-directory']
+
 
         environment_section = options.get('environment')
         if environment_section:
@@ -40,6 +42,24 @@ class Base:
         options['_environment-data'] = repr(environment_data)
 
         self.build_ext = build_ext(buildout, options)
+
+        links = options.get('find-links',
+                            buildout['buildout'].get('find-links'))
+        if links:
+            links = links.split()
+            options['find-links'] = '\n'.join(links)
+        else:
+            links = ()
+        self.links = links
+
+        index = options.get('index', buildout['buildout'].get('index'))
+        if index is not None:
+            options['index'] = index
+        self.index = index
+
+        self.newest = buildout['buildout'].get('newest') == 'true'
+
+
 
     def install(self):
         self._set_environment()
@@ -119,26 +139,8 @@ class Custom(Base):
     def __init__(self, buildout, name, options):
         Base.__init__(self, buildout, name, options)
 
-        links = options.get('find-links',
-                            buildout['buildout'].get('find-links'))
-        if links:
-            links = links.split()
-            options['find-links'] = '\n'.join(links)
-        else:
-            links = ()
-        self.links = links
-
-        index = options.get('index', buildout['buildout'].get('index'))
-        if index is not None:
-            options['index'] = index
-        self.index = index
-
-        options['_e'] = buildout['buildout']['eggs-directory']
-
         if buildout['buildout'].get('offline') == 'true':
             self._install = lambda: ()
-
-        self.newest = buildout['buildout'].get('newest') == 'true'
 
     def _install(self):
         options = self.options