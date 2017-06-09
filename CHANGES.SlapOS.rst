7a5dd160e642ec4a387baab128208b904704db97
Julien Muchembled <jm@nexedi.com>
2017-06-09 17:50:41 +0200

    Release 2.5.2+slapos009

--- CHANGES.SlapOS.rst
+++ CHANGES.SlapOS.rst
@@ -1,3 +1,8 @@
+2.5.2+slapos009
+---------------
+
+- More fixes for Python 3
+
 2.5.2+slapos008
 ---------------
 
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 name = "zc.buildout"
-version = '2.5.2+slapos008'
+version = '2.5.2+slapos009'
 
 import os
 from setuptools import setup
